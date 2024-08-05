f675107bfd1378d6ce232c1f5388f020398a1de5
tejaswinimada <125640099+tejaswinimada@users.noreply.github.com>
2024-08-05 15:02:24 +0530

    Add files via upload
    Here is my creative portfolio developed and designed using HTML,CSS,JAVASCRIPT

---
+++ port.js
@@ -0,0 +1,8 @@
+var typed=new Typed(".text",{
+    strings:["Backend developer","Web Developer","Creative Thinker"],
+    typeSpeed:100,
+    backSpeed:100,
+    backdelay:1000,
+    loop:true
+
+});
